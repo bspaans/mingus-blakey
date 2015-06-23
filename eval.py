3a2a4e23480de401e79de4c5540d5506dbfe9e97
Bart Spaans <bart.spaans@gmail.com>
2015-06-23 18:31:04 +0100

    converting into mingus tracks

--- eval.py
+++ eval.py
@@ -2,13 +2,17 @@
 
 import yacc
 import sys
+from mingus.containers.instrument import MidiPercussionInstrument
+from mingus.containers import Track, Bar, NoteContainer, Note
 
 class Context(object):
     def __init__(self):
         self.context = {}
-        self.attr = {}
+        self.attr = {"resolution": 8, "bpm": 120}
     def set_attr(self, name, value):
         self.attr[name] = value
+    def get_attr(self, name):
+        return self.attr[name]
     def set(self, name, pattern):
         self.context[name] = pattern
     def get(self, name):
@@ -52,12 +56,42 @@ def eval_sequence(ctx, sequence):
         result.extend(ptrn)
     ctx.set(sequence.name, result)
 
-def output_result_of_last_statement(ctx, statement):
+def get_result_of_last_statement(ctx, statement):
     if statement is None:
         print "Nothing to do, bozo"
         sys.exit(0)
-    print ctx.get(statement)
+    stmt = ctx.get(statement)
+    return convert_pattern_to_mingus_track(ctx, stmt)
 
+percussion = MidiPercussionInstrument()
+def convert_pattern_char_to_note(char):
+    if char == 'h':
+        return percussion.closed_hi_hat()
+    elif char == '-':
+        return None
+    elif char == 'b':
+        return percussion.bass_drum_1()
+    elif char == 's':
+        return percussion.acoustic_snare()
+    elif char == 'C':
+        return percussion.crash_cymbal_1()
+    print "Error: unknown symbol", char
+    sys.exit(1)
+
+
+def convert_pattern_to_mingus_track(ctx, pattern):
+    resolution = ctx.get_attr("resolution")
+    result = Track()
+    result.instrument = MidiPercussionInstrument()
+    for beat in pattern:
+        nc = NoteContainer()
+        if beat is None:
+            result.add_notes(nc, resolution)
+            continue
+        for b in beat:
+            nc.add_note(convert_pattern_char_to_note(b))
+        result.add_notes(nc, resolution)
+    return result
 
 def eval_statements(statements):
     ctx = Context()
@@ -71,7 +105,7 @@ def eval_statements(statements):
         elif type(statement) is yacc.Sequence:
             eval_sequence(ctx, statement)
             last_statement = statement.name
-    output_result_of_last_statement(ctx, last_statement)
+    return get_result_of_last_statement(ctx, last_statement)
 
 def eval_file(file):
     eval_statements(yacc.parse_file(file))
